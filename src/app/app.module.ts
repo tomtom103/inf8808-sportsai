dac162ef1b56b66d842b762e6606e9bf0fd1143a
Thomas Caron <thomas.caron@polymtl.ca>
2022-06-13 22:33:36 -0400

    Bring back bar-chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
 import { AppComponent } from '@app/pages/app/app.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { ScatterComponent } from './components/scatter/scatter.component';
 import { AppMaterialModule } from './modules/material.module';
 import { HomePageComponent } from './pages/home/home.component';
 
 @NgModule({
-    declarations: [AppComponent, HomePageComponent, ScatterComponent, PieChartComponent],
+    declarations: [AppComponent, HomePageComponent, ScatterComponent, BarChartComponent, PieChartComponent],
     imports: [AppMaterialModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+
+@Component({
+    selector: 'app-bar-chart',
+    templateUrl: './bar-chart.component.html',
+    styleUrls: ['./bar-chart.component.scss'],
+})
+export class BarChartComponent implements OnInit {
+    private data = [
+        { framework: 'Vue', stars: 166443, released: 2014 },
+        { framework: 'React', stars: 150793, released: 2013 },
+        { framework: 'Angular', stars: 62342, released: 2016 },
+        { framework: 'Backbone', stars: 27647, released: 2010 },
+        { framework: 'Ember', stars: 21471, released: 2011 },
+    ];
+    private svg!: any;
+    private margin: number = 50;
+    private width: number = 750 - this.margin * 2;
+    private height: number = 400 - this.margin * 2;
+
+    ngOnInit(): void {
+        this.createSvg();
+        this.drawBars(this.data);
+    }
+
+    private createSvg(): void {
+        this.svg = d3
+            .select('figure#bar')
+            .append('svg')
+            .attr('width', this.width + this.margin * 2)
+            .attr('height', this.height + this.margin * 2)
+            .append('g')
+            .attr('transform', 'translate(' + this.margin + ',' + this.margin + ')');
+    }
+
+    private drawBars(data: any[]): void {
+        // Create the X-axis band scale
+        const x = d3
+            .scaleBand()
+            .range([0, this.width])
+            .domain(data.map((d) => d.framework))
+            .padding(0.2);
+
+        // Draw the X-axis on the DOM
+        this.svg
+            .append('g')
+            .attr('transform', 'translate(0,' + this.height + ')')
+            .call(d3.axisBottom(x))
+            .selectAll('text')
+            .attr('transform', 'translate(-10,0)rotate(-45)')
+            .style('text-anchor', 'end');
+
+        // Create the Y-axis band scale
+        const y = d3.scaleLinear().domain([0, 200000]).range([this.height, 0]);
+
+        // Draw the Y-axis on the DOM
+        this.svg.append('g').call(d3.axisLeft(y));
+
+        // Create and fill the bars
+        this.svg
+            .selectAll('bars')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', (d: any) => x(d.framework))
+            .attr('y', (d: any) => y(d.stars))
+            .attr('width', x.bandwidth())
+            .attr('height', (d: any) => this.height - y(d.stars))
+            .attr('fill', '#d04a35');
+    }
+}
