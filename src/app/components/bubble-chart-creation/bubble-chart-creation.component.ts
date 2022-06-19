09421f59091f9f18a5e58ab896215381543e0d19
Slimane <slimaneb2000@gmail.com>
2022-06-19 19:14:12 -0400

    adding legend for bubblechart

--- src/app/components/bubble-chart-creation/bubble-chart-creation.component.ts
+++ src/app/components/bubble-chart-creation/bubble-chart-creation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { legendSize } from 'd3-svg-legend';
 
 @Component({
     selector: 'app-bubble-chart-creation',
@@ -8,12 +9,12 @@ import * as d3 from 'd3';
 })
 export class BubbleChartCreationComponent implements OnInit {
     private data = [
-        { player: 'Sadio Mané', KP: 36, PPA: 54, GCA: 13 },
-        { player: 'Karim Benzema', KP: 61, PPA: 84, GCA: 21 },
-        { player: 'Rafael Leao', KP: 45, PPA: 51, GCA: 21 },
-        { player: 'Mohamed Salah', KP: 61, PPA: 61, GCA: 25 },
-        { player: 'Kylian Mbappé', KP: 70, PPA: 60, GCA: 38 },
-        { player: 'Christopher Nkunku', KP: 63, PPA: 58, GCA: 28 },
+        { player: 'Mané', KP: 36, PPA: 54, GCA: 13 },
+        { player: 'Benzema', KP: 61, PPA: 84, GCA: 21 },
+        { player: 'Leao', KP: 45, PPA: 51, GCA: 21 },
+        { player: 'Salah', KP: 61, PPA: 61, GCA: 25 },
+        { player: 'Mbappé', KP: 70, PPA: 60, GCA: 38 },
+        { player: 'Nkunku', KP: 63, PPA: 58, GCA: 28 },
     ];
 
     private svg!: any;
@@ -47,16 +48,11 @@ export class BubbleChartCreationComponent implements OnInit {
         const y = d3.scaleLinear().domain([45, 90]).range([this.height, 0]);
         this.svg.append('g').call(d3.axisLeft(y));
 
-        // Add color scale
-        const color = d3
-            .scaleOrdinal()
-            .domain(this.data.map((d) => d.player))
-            .range(d3.schemeCategory10);
-
+        let gca = this.data.map((d) => d.GCA);
         const radius = d3
             .scaleLinear()
-            .domain(this.data.map((d) => d.GCA))
-            .range([5, 10]);
+            .domain([d3.min(gca as any[]), d3.max(gca as any[])])
+            .range([5, 15]);
 
         const dots = this.svg.append('g');
         dots.selectAll('dot')
@@ -66,7 +62,32 @@ export class BubbleChartCreationComponent implements OnInit {
             .attr('cx', (d) => x(d.KP))
             .attr('cy', (d) => y(d.PPA))
             .attr('r', (d) => radius(d.GCA))
-            .style('opacity', 0.5)
-            .style('fill', (d) => color(d.player));
+            .style('opacity', 0.9)
+            .style('fill', (d) => '#21A179');
+
+        dots.selectAll('text')
+            .data(this.data)
+            .enter()
+            .append('text')
+            .text((d) => d.player)
+            .attr('x', (d) => x(d.KP) - 10)
+            .attr('y', (d) => y(d.PPA) - 10)
+            .style('font-size', 12);
+
+        //axis
+        this.svg.append('text').text('PPA').attr('class', 'y axis-text').attr('transform', 'rotate(-90)').attr('font-size', 12);
+
+        this.svg.append('text').text('KP (Key passes)').attr('class', 'x axis-text').attr('font-size', 12);
+
+        this.svg.selectAll('.x.axis-text').attr('transform', `translate(${this.width / 2 - 40}, ${this.height + 40})`);
+
+        this.svg.selectAll('.y.axis-text').attr('transform', `translate(-40, ${this.height / 2}) rotate(-90)`);
+
+        //legend size
+        this.svg.append('g').attr('class', 'radius').attr('transform', 'translate(700, 10)');
+
+        var legend = legendSize().shape('circle').shapePadding(15).labelOffset(20).orient('vertical').scale(radius);
+
+        this.svg.select('.radius').style('fill', '#21A179').style('font-size', 11).call(legend);
     }
 }
