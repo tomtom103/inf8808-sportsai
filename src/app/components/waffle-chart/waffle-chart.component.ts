f20565980151883789ae2031a9edb4f34c0849f4
Thomas Caron <thomas.caron@polymtl.ca>
2022-06-20 18:57:12 -0400

    Basic skeleton

--- src/app/components/waffle-chart/waffle-chart.component.ts
+++ src/app/components/waffle-chart/waffle-chart.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
-  selector: 'app-waffle-chart',
-  templateUrl: './waffle-chart.component.html',
-  styleUrls: ['./waffle-chart.component.scss']
+    selector: 'app-waffle-chart',
+    templateUrl: './waffle-chart.component.html',
+    styleUrls: ['./waffle-chart.component.scss'],
 })
 export class WaffleChartComponent implements OnInit {
+    private className: string = 'waffle-chart';
+    private data: any[] = [];
+    private svg: any;
 
-  constructor() { }
+    plotSize: number = 750;
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {
+        this.createSvg();
+    }
+
+    private createSvg(): void {
+        this.svg = d3
+            .select('figure#offensive-danger')
+            .append('svg')
+            .attr('class', this.className)
+            .attr('width', this.plotSize)
+            .attr('height', this.plotSize);
+    }
 }
