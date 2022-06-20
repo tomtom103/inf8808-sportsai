752229e9c52ed534bad10ecc4671d1e6866a0db5
Thomas Caron <thomas.caron@polymtl.ca>
2022-06-20 19:01:27 -0400

    Added waffle-chart data

--- src/app/components/waffle-chart/waffle-chart.component.ts
+++ src/app/components/waffle-chart/waffle-chart.component.ts
@@ -8,7 +8,44 @@ import * as d3 from 'd3';
 })
 export class WaffleChartComponent implements OnInit {
     private className: string = 'waffle-chart';
-    private data: any[] = [];
+    private data: any[] = [
+        {
+            Player: 'Sadio Mané',
+            Gls: 16,
+            Sh: 99,
+            SoT: 37,
+        },
+        {
+            Player: 'Rafael Leão',
+            Gls: 11,
+            Sh: 97,
+            SoT: 38,
+        },
+        {
+            Player: 'Karim Benzema',
+            Gls: 27,
+            Sh: 118,
+            SoT: 47,
+        },
+        {
+            Player: 'Mohamed Salah',
+            Gls: 23,
+            Sh: 134,
+            SoT: 49,
+        },
+        {
+            Player: 'Kylian Mbappé',
+            Gls: 28,
+            Sh: 144,
+            SoT: 60,
+        },
+        {
+            Player: 'Christopher Nkunku',
+            Gls: 20,
+            Sh: 80,
+            SoT: 39,
+        },
+    ];
     private svg: any;
 
     plotSize: number = 750;
