c06b09880a27a1325d750fa43bf768a0e313f2cc
MohamedAli-M <mohamed-ali.messedi@polymtl.ca>
2022-06-24 12:09:11 -0400

    Added missing component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-home-page',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+    ngOnInit(): void {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        };
+    }
+}
